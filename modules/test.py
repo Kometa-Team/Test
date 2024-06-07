f30fc286ba2d54e29f878dbfeb5f9ac8935e85ab
meisnate12 <meisnate12@gmail.com>
2024-06-07 13:53:18 -0400

    sdf

--- modules/test.py
+++ modules/test.py
@@ -1,1 +1,13 @@
-print("test6leshjfhdg")
+import ruamel.yaml, os
+
+path = "test.yml"
+
+yaml = ruamel.yaml.YAML(typ='safe')
+if not os.path.exists(path):
+    with open(path, 'w'):
+        pass
+
+data = {"test": {"one": None, "two": "", "three": "null"}}
+
+with open(path, 'w', encoding="utf-8") as fp:
+    yaml.dump(data, fp)
